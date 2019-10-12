10a9da575f3104919a6d88ec6ffe1011e6a76ba5
zhukehui <574059694@qq.com>
2019-10-12 20:30:15 +0800

    Set多线程并发示例

--- src/main/java/com/atguigu/set/HashSetConcurrent.java
+++ src/main/java/com/atguigu/set/HashSetConcurrent.java
@@ -5,9 +5,6 @@ import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 
-
-
-
 /**
  * 
  * 多线程：线程    操作    资源类
@@ -43,28 +40,4 @@ public class HashSetConcurrent {
 	}
 }
 
-/**
- * 写时复制
- CopyOnWrite容器即写时复制的容器。往一个容器添加元素的时候，不直接往当前容器Object[]添加，而是先将当前容器Object[]进行Copy，
- 复制出一个新的容器Object[] newElements，然后新的容器Object[] newElements里添加元素，添加完元素之后，
- 再将原容器的引用指向新的容器 setArray(newElements);。这样做的好处是可以对CopyOnWrite容器进行并发的读，
- 而不需要加锁，因为当前容器不会添加任何元素。所以CopyOnWrite容器也是一种读写分离的思想，读和写不同的容器
- public boolean add(E e)
- {
- final ReentrantLock lock = this.lock;
 
- lock.lock();
-
- try{
- 	Object[] ele ments = getArray();
- 	int len = elements.length;
- 	Object[] newElements = Arrays.copyOf(elements, len + 1);
- 	newElements[len] = e;
- 	setArray(newElements);
- 	return true;
- }
- finally {
- 	lock.unlock();
- 	}
- }
- */
